ae633dfd41e0a8e4b760554cfb46f7dfa97d9323
Phil <happy.melodist@gmail.com>
2021-12-27 00:14:35 -0600

    write async await and fetch functionality for the app

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
-// http://numbersapi.com/
+// // http://numbersapi.com/
+
+// const factBtn = document.querySelector("button");
+// const factInput = document.querySelector("input");
+// const factPara = document.querySelector("p");
+
+// factBtn.addEventListener("click", () => {
+//   const number = factInput.value;
+//   const numberFact = parseInt(number);
+
+//   fetchFact(numberFact);
+// });
+
+// async function fetchFact(number) {
+//   const factURL = "http://numbersapi.com/";
+//   const proxyURL = "https://cors-anywhere.herokuapp.com/";
+
+//   const response = await fetch(`${proxyURL}${factURL}${number}`, {
+//     method: "GET",
+//     headers: {
+//       "x-requested-with": "text/plain",
+//     },
+//   });
+
+//   const responseData = await response.text();
+
+//   factPara.innerText = responseData;
+// }
+
+// to thine own self be true
+
+/*
+my code below
+*/
 
 const factBtn = document.querySelector("button");
 const factInput = document.querySelector("input");
@@ -11,18 +44,17 @@ factBtn.addEventListener("click", () => {
   fetchFact(numberFact);
 });
 
-async function fetchFact(number) {
+async function fetchFact(boobies) {
   const factURL = "http://numbersapi.com/";
   const proxyURL = "https://cors-anywhere.herokuapp.com/";
 
-  const response = await fetch(`${proxyURL}${factURL}${number}`, {
-    method: "GET",
+  const response = await fetch(`${proxyURL}${factURL}${boobies}`, {
+    method: "get",
     headers: {
       "x-requested-with": "text/plain",
     },
   });
 
   const responseData = await response.text();
-
   factPara.innerText = responseData;
 }
